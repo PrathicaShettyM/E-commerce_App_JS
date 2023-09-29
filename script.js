97829da60f5bda090d5206996d913a4a8e9f1096
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-09-29 09:04:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,28 @@
             let products = document.querySelector('.products')
 
             async function fetchProducts(url){
-            let data = await fetch(url);
-            let response = await data.json();
+                try {
+                let data = await fetch(url);
+                let response = await data.json();
 
-            for(let i = 0; i < response.length; i++){
-            products.innerHTML += `
-            <div class="product">
-                <img src="" alt="" class="product-img">
-                <h2 class="product-title">${response[i].title}</h2>
-                <h4 class="product-category">${response[i].category}</h4>
-                <p class="product-description">${response[i].description}</p>
-                <div class="product-price-container">
-                    <h3 class="product-price">$${response[i].price}</h3>
-                    <a href="#!" data-productId="" class="add-to-cart"></a>
-                </div>
-            `;
+                for(let i = 0; i < response.length; i++){
+                products.innerHTML += `
+                <div class="product">
+                    <img src="${response[i].images[1]}" alt="" class="product-img">
+                    <h2 class="product-title">${response[i].title}</h2>
+                    <h4 class="product-category">${response[i].category}</h4>
+                    <p class="product-description">${response[i].description}</p>
+                    <div class="product-price-container">
+                        <h3 class="product-price">$${response[i].price}</h3>
+                        <a href="#!" data-productId="" class="add-to-cart"></a>
+                    </div>
+                `;
+                }
+                console.log(response);
+                } catch (error) {
+                console.log(error);
+                }        
+             }
 
-            }
-
-            
-            console.log(response);
-            }
             fetchProducts('https://api.escuelajs.co/api/v1/products')
         })
