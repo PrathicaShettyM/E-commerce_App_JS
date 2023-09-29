b84e885ca7a82fadbeae17b05472d449ce4a54af
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-09-29 08:52:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+  document.addEventListener('DOMContentLoaded', function () {
 
+            let products = document.querySelector('.products')
+
+            async function fetchProducts(url){
+            let data = await fetch(url);
+            let response = await data.json();
+
+            for(let i = 0; i < response.length; i++){
+            products.innerHTML += `
+            <div class="product">
+                <img src="" alt="" class="product-img">
+                <h2 class="product-title">${response[i].title}</h2>
+                <h4 class="product-category">${response[i].category}</h4>
+                <p class="product-description">${response[i].description}</p>
+                <div class="product-price-container">
+                    <h3 class="product-price">$${response[i].price}</h3>
+                    <a href="#!" data-productId="" class="add-to-cart"></a>
+                </div>
+            `;
+
+            }
+
+            
+            console.log(response);
+            }
+            fetchProducts('https://api.escuelajs.co/api/v1/products')
+        })
